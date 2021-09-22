11a3dfe5c4e3ac779121129b273030b1e39efe53
Elefly13 <elymara13@gmail.com>
2021-09-22 17:57:42 +0200

    fine header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    
+    <Header/>
     <Main/>
   </div>
 </template>
 
 <script>
 import Main from './components/Main.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    Main
+    Main,
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="box-header">
+  <div class="logo">
+    <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/file-spotify-logo-png-4.png" alt="Logo Spotify">
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../style/general.scss";
+@import "../style/vars.scss";
+.box-header {
+  width: 100%;
+  height: 80px;
+  background-color:$primo;
+  padding: 15px 30px;
+    .logo {
+      width: 50px;
+      height: 50px;
+     
+
+        img {
+          width: 100%;
+          
+        }
+    }
+}
+
+</style>
