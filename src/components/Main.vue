9645187b70e0ee5660b4cb48eea31b561bd51c77
Elefly13 <elymara13@gmail.com>
2021-09-22 20:38:44 +0200

    fine esercizio

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,54 @@
 <template>
 <div>
+  <div class="container py-5">
+    <div class="row gx-5">
+      <div v-for="(disco, index) in dischiList" :key="index" class="col-6 col-md-4 col-lg-2 m-3">
+       <MainCard :info="disco"/>
+      </div>
+    </div>
+  </div>
 
 </div>
 </template>
 
 <script>
+import axios from 'axios';
+import MainCard from './MainCard.vue'
 export default {
   name: 'Main',
-  
-    
-}
+  components: {
+    MainCard
+  },
+  data() {
+    return {
+      APIUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+      dischiList: []
+      
+    }
+  },
+  created() {
+    this.getDischi();
+  },
+  methods: {
+    getDischi() {
+      axios
+          .get(this.APIUrl)
+          .then( resp => {
+            // console.log(resp.data);
+            this.dischiList = resp.data.response;
+            
+          })
+          
+    }
+  }
+
+}  
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+div {
+  color: black;
+}
 </style>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <div class="card p-3" style="width: 230px; height: 380px;">
+    <div class="card-img align-center" style="width: 200px; height: 200px;">
+      <img :src="info.poster" :alt="info.title" class="card-img-top " style="width: 200px; height: 200px;">
+    </div>
+    
+    <div class="card-body text-center">
+      <h5 class="card-title ">{{info.title}}</h5>
+      <p class="card-text">{{info.author}}</p>
+      <p class="card-text">{{info.year}}</p>
+    </div>
+</div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MainCard',
+  props: ['info']
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../style/vars.scss";
+.card {
+  background-color: $primo;
+    h5 {
+      color:$terzo;
+      font-weight: 900;
+    }
+    p {
+      color: $quarto;
+    }
+}
+
+</style>
